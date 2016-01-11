cfbb006fa18980c7c6e9a118c6be7ef3d4715829
Hitesh MAC <hiteshmac@Hiteshs-MacBook-Pro.local>
2016-01-10 22:19:52 -0500

    final code

---
+++ promise.js
@@ -0,0 +1,19 @@
+var http = require("http");
+var url = require("url");
+var Q = require("q");
+
+var httpGet = function (opts) {
+    var deferred = Q.defer();
+    http.get(opts, deferred.resolve);
+    return deferred.promise;
+};
+
+httpGet(url.parse("http://example.org/")).then(function (res) {
+    console.log(res.statusCode);  // maybe 302
+    console.log('here');
+
+    //return httpGet(url.parse(res.headers["location"]));
+}).then(function (res) {
+  console.log('+++++');
+    console.log(res.statusCode);  // maybe 200
+});
